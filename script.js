780c289fe5d83ba62631f0c34c5f4b6a0ac528cf
nodirbeknorqobulov <nodirit18@gmail.com>
2024-01-04 15:57:44 +0300

    sinfishi

---
+++ script.js
@@ -0,0 +1,128 @@
+console.log("salom dunyo")
+
+
+let age = "hello"
+
+// ? CamelCase
+let colorCar = "red";
+
+// Data types
+// Malumot turlari
+// ? String
+let str = "Salom"
+let str2 = 'Dunyo'
+
+let longStr = 'mening ismim "Nodirbek"'
+
+// ? Number
+let firstNumber = 4
+let secondNum = 10
+
+let strNum = "4"
+
+// ? Boolean
+let isTrue = true
+let isFalse = false
+
+
+// ? undefined 
+
+let und;
+
+console.log(und);
+
+// ? Null
+let isNull = null;
+
+console.log(isNull);
+
+// ? Typeof
+console.log(typeof isTrue);
+
+// ? Object
+
+let person = {
+    name: "Shaxzod",
+    age: 25,
+    weight: "100",
+    height: 1.80,
+}
+
+let phone = {
+    model: "iphone",
+    color: "red",
+    weight: 200,
+}
+
+// ? Object
+// ? array
+
+let arr = [1,5,7,3,6,8]
+
+console.log(typeof arr);
+
+// console.log(shaxzod.weight+ " kg")
+
+// console.log(5*5);
+// document.body.innerHTML = firstNumber
+
+// ? Operatorlar 
+
+let a = 30;
+
+a = 40;
+let b = 10;
+
+// ? + Addition
+
+let c = a + b;
+
+let str1 = "salom";
+
+let strUnd = "nimadir";
+
+console.log(str1 - strUnd);
+
+// ? - Minus
+
+console.log("result", a - b);
+
+// ? * Multiplication
+
+let resMultiple = a * b;
+
+console.log(resMultiple);
+
+// ? / Division
+
+let div = a / b;
+
+console.log(div);
+
+// ? % Qoldiqli bo'lish
+
+console.log(45 % b);
+
+let d = 30;
+
+// d = d + a; === d += a;
+d += a; // d = d+a;
+
+// string
+let z = 20;
+
+// number
+let x = 20;
+
+// ? Tenglik
+// ==
+// ===
+
+//? ! emas manosida keladi
+
+
+// console.log(z < x);
+console.log(z !== x);
+
+// ? Darajaga kutarish
+console.log(z**3);
